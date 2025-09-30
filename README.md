792d96867ece3d28e0c6f5d353b16ff999764553
tremorscript <tremorscipt@gmail.com>
2025-09-30 11:11:54 +0530

    commit changes

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# Platy.AdventureWorks.Repository
+[![Build Project](https://github.com/tremorscript/Platy.AdventureWorks.Repository/actions/workflows/ci.yml/badge.svg)](https://github.com/tremorscript/Platy.AdventureWorks.Repository/actions/workflows/ci.yml)
+
+[![NuGet Version](https://img.shields.io/nuget/v/Platy.AdventureWorks.Repository?label=Platy.AdventureWorks.Repository)](https://www.nuget.org/packages/Platy.AdventureWorks.Repository/)
+
+# Overview
 This is the project template that I use to quickly create a set of repository classes for interacting with any existing database
 
-## Features
+# Features
 - The DbContext and Entity classes are internal, which means that external libraries users cannot make db changes without interacting with the repository classes.
 - The repository classes validate the entities before making changes to the database
 - A Result type is returned in case of success or failure, instead of relying on exceptions.
 - A notification is published for every Create, Update and Delete event. External libraries can subscribe to these events.
 - `csx` templates help generate the code for existing databases to help get started quickly. Uses a forked version of [Loresoft EntityFrameworkGenerator](https://github.com/loresoft/EntityFrameworkCore.Generator).
-- Intgration tests using TestContainers.
+- Integration tests using TestContainers.
--- tests/Platy.AdventureWorks.Repository.UnitTests/UnitTest1.cs
+++ tests/Platy.AdventureWorks.Repository.UnitTests/UnitTest1.cs
@@ -1,19 +1,11 @@
-﻿using Platy.AdventureWorks.Repository.Data;
-using Platy.AdventureWorks.Repository.Domain.Mapping;
-using Platy.AdventureWorks.Repository.Domain.Models;
-using Platy.AdventureWorks.Repository.Domain.Validation;
-using Ardalis.SharedKernel;
-using AutoMapper;
+﻿using AutoMapper;
 using MediatR;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NSubstitute;
-using Microsoft.Extensions.Configuration;
 using Platy.AdventureWorks.Repository.Domain.Mapping;
 using Platy.AdventureWorks.Repository.Domain.Models;
 using Platy.AdventureWorks.Repository.Domain.Validation;
-using Platy.AdventureWorks.Repository;
 
 namespace Platy.AdventureWorks.Repository.UnitTests;
 
@@ -22,40 +14,39 @@ public class UnitTest1
   [Fact]
   public async void Test1()
   {
-    
-    
-    // var config = new ConfigurationBuilder()
-    //   .AddUserSecrets<UnitTest1>()
-    //   .Build();
-    //
-    // var serviceCollection = new ServiceCollection();
-    // serviceCollection.AddAdventureWorkDatabase($"{config["AdventureWorksDb"]}");
-    //
-    //
-    // var serviceProvider = serviceCollection.BuildServiceProvider();
-    //  var configuration = new MapperConfiguration(cfg => {
-    //      cfg.AddProfile<AddressProfile>();
-    //  });
-    //  var mapper = new Mapper(configuration);
-    //
-    // AddressRepository test = new AddressRepository(
-    //   serviceProvider,
-    //   mapper,
-    // serviceProvider.GetService<IMediator>(),
-    // serviceProvider.GetService<ILogger<AddressRepository>>(),
-    //   new AddressCreateModelValidator(),
-    //   new AddressUpdateModelValidator()
-    //   );
-    //
-    // var readModel = await test.GetAsync(1, CancellationToken.None);
-    // AddressCreateModel newModel = new AddressCreateModel()
-    // {
-    //   AddressLine1 = readModel.Value.AddressLine2 + "test",
-    //   AddressLine2 = readModel.Value.AddressLine2 + "test",
-    //   City = readModel.Value.City,
-    //   PostalCode =  readModel.Value.PostalCode,
-    //   StateProvinceID = readModel.Value.StateProvinceID 
-    // } ;
-    // await test.CreateAsync(newModel,CancellationToken.None);
+    var config = new ConfigurationBuilder()
+      .AddUserSecrets<UnitTest1>()
+      .Build();
+
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddAdventureWorkDatabase($"{config["AdventureWorksDb"]}");
+
+
+    var serviceProvider = serviceCollection.BuildServiceProvider();
+    var configuration = new MapperConfiguration(cfg =>
+    {
+      cfg.AddProfile<AddressProfile>();
+    });
+    var mapper = new Mapper(configuration);
+
+    var test = new AddressRepository(
+      serviceProvider,
+      mapper,
+      serviceProvider.GetService<IMediator>(),
+      serviceProvider.GetService<ILogger<AddressRepository>>(),
+      new AddressCreateModelValidator(),
+      new AddressUpdateModelValidator()
+    );
+
+    var readModel = await test.GetAsync(1, CancellationToken.None);
+    var newModel = new AddressCreateModel
+    {
+      AddressLine1 = readModel.Value.AddressLine2 + "test",
+      AddressLine2 = readModel.Value.AddressLine2 + "test",
+      City = readModel.Value.City,
+      PostalCode = readModel.Value.PostalCode,
+      StateProvinceId = readModel.Value.StateProvinceId
+    };
+    await test.CreateAsync(newModel, CancellationToken.None);
   }
 }
