7bb85335ae5e54bd81f3e3da7f2873348c51e805
tremorscript <tremorscipt@gmail.com>
2025-10-04 17:22:28 +0530

    commit changes

--- tests/Platy.AdventureWorks.Repository.Tests.Integration/AddressRepositoryTest.cs
+++ tests/Platy.AdventureWorks.Repository.Tests.Integration/AddressRepositoryTest.cs
@@ -1,9 +1,5 @@
 ﻿using Ardalis.Result;
-using AutoMapper;
-using FluentValidation;
-using MediatR;
-using Microsoft.Extensions.Logging;
-using NSubstitute;
+using Microsoft.Extensions.DependencyInjection;
 using Platy.AdventureWorks.Repository.Domain.Models;
 using Shouldly;
 
@@ -14,73 +10,75 @@ namespace Platy.AdventureWorks.Repository.Tests.Integration;
 ///   Methods should follow the naming convention:- [MethodWeTest_StateUnderTest_ExpectedBehavior]
 ///   [HTTPMethod]_[MethodName]_[StateUnderTest]_ThenTheAPIReturnsExpected[Http_Code]
 /// </summary>
+[Collection("Database collection")]
 public class AddressRepositoryTest
 {
-  private readonly IValidator<AddressCreateModel> _createValidator = Substitute.For<IValidator<AddressCreateModel>>();
-  private readonly ILogger<AddressRepository> _logger = Substitute.For<ILogger<AddressRepository>>();
-  private readonly IMapper _mapper = Substitute.For<IMapper>();
-  private readonly IMediator _mediator = Substitute.For<IMediator>();
-  private readonly IServiceProvider _serviceProvider = Substitute.For<IServiceProvider>();
-  private readonly AddressRepository _sut;
-  private readonly IValidator<AddressUpdateModel> _updateValidator = Substitute.For<IValidator<AddressUpdateModel>>();
+  private readonly DatabaseFixture _fixture;
+  private readonly ServiceProvider _serviceProvider;
 
-  public AddressRepositoryTest()
+  public AddressRepositoryTest(DatabaseFixture fixture)
   {
-    _sut = new AddressRepository(_serviceProvider, _mapper, _mediator, _logger, _createValidator, _updateValidator);
+    _fixture = fixture;
+    _serviceProvider = fixture.ServiceProvider;
   }
 
   [Fact]
   public async Task GetAsync_ValidAddressId_ReturnsValidAddress()
   {
-    // var config = new ConfigurationBuilder()
-    //   .AddUserSecrets<AddressRepositoryTest>()
-    //   .Build();
-    //
-    // var serviceCollection = new ServiceCollection();
-    // serviceCollection.AddAdventureWorkDatabase($"{config["AdventureWorksDb"]}");
-    //
-    // var configuration = new MapperConfiguration(cfg =>
-    // {
-    //   cfg.AddProfile<AddressProfile>();
-    // });
-    // var mapper = new Mapper(configuration);
-    // serviceCollection.AddAutoMapper(cfg => { }, typeof(AddressProfile));
-    //
-    // var serviceProvider = serviceCollection.BuildServiceProvider();
-
-
-    // var test = serviceProvider.GetService<IAddressRepository>();
-    var test = Substitute.For<IAddressRepository>();
-
     // Arrange
-    test.GetAsync(123, CancellationToken.None)
-      .Returns(new Result<AddressReadModel>(new AddressReadModel { Id = 123 }));
+    var addressRepository = _serviceProvider.GetService<IAddressRepository>();
 
     // Act
-    var test1 = await test.GetAsync(123, CancellationToken.None);
+    var addressReadModel = await addressRepository!.GetAsync(123, CancellationToken.None);
 
     // Assert
-    test1.Value.Id.ShouldBe(123);
+    addressReadModel.Value.Id.ShouldBe(123);
+  }
 
+  [Fact]
+  private async Task CreateAsync_InvalidPostalCode_ReturnsValidationErrors()
+  {
+    // Arrange
+    var addressRepository = _serviceProvider.GetService<IAddressRepository>();
+    var readModel = await addressRepository!.GetAsync(1, CancellationToken.None);
+    var newModel = new AddressCreateModel
+    {
+      AddressLine1 = readModel.Value.AddressLine2 + "test" + Random.Shared.NextInt64(),
+      AddressLine2 = readModel.Value.AddressLine2 + "test" + Random.Shared.NextInt64(),
+      City = readModel.Value.City + "test" + Random.Shared.NextInt64(),
+      PostalCode = readModel.Value.PostalCode + "test" + Random.Shared.NextInt64(),
+      StateProvinceId = readModel.Value.StateProvinceId
+    };
 
-    // var test = new AddressRepository(
-    //   serviceProvider,
-    //   mapper,
-    //   serviceProvider.GetService<IMediator>(),
-    //   serviceProvider.GetService<ILogger<AddressRepository>>(),
-    //   new AddressCreateModelValidator(),
-    //   new AddressUpdateModelValidator()
-    // );
-    //
-    // var readModel = await test.GetAsync(1, CancellationToken.None);
-    // var newModel = new AddressCreateModel
-    // {
-    //   AddressLine1 = readModel.Value.AddressLine2 + "test",
-    //   AddressLine2 = readModel.Value.AddressLine2 + "test",
-    //   City = readModel.Value.City,
-    //   PostalCode = readModel.Value.PostalCode,
-    //   StateProvinceId = readModel.Value.StateProvinceId
-    // };
-    // await test.CreateAsync(newModel, CancellationToken.None);
+    // Act
+    var addressReadModel = await addressRepository.CreateAsync(newModel, CancellationToken.None);
+
+    // Assert
+    addressReadModel.IsInvalid().ShouldBe(true);
+    addressReadModel.ValidationErrors.Count().ShouldBe(1);
+  }
+
+  [Fact]
+  private async Task CreateAsync_ValidAddressValues_ReturnsSuccess()
+  {
+    // Arrange
+    var addressRepository = _serviceProvider.GetService<IAddressRepository>();
+    var readModel = await addressRepository!.GetAsync(1, CancellationToken.None);
+    var newModel = new AddressCreateModel
+    {
+      AddressLine1 = readModel.Value.AddressLine2 + "test" + Random.Shared.NextInt64(),
+      AddressLine2 = readModel.Value.AddressLine2 + "test" + Random.Shared.NextInt64(),
+      City = readModel.Value.City + "test" + Random.Shared.NextInt64(),
+      PostalCode = readModel.Value.PostalCode,
+      StateProvinceId = readModel.Value.StateProvinceId
+    };
+
+    // Act
+    var addressReadModel = await addressRepository.CreateAsync(newModel, CancellationToken.None);
+
+    // Assert
+    addressReadModel.IsInvalid().ShouldBe(false);
+    addressReadModel.Errors.Count().ShouldBe(0);
+    addressReadModel.Value.AddressLine1.ShouldBe(newModel.AddressLine1);
   }
 }
--- tests/Platy.AdventureWorks.Repository.Tests.Integration/DatabaseFixture.cs
+++ tests/Platy.AdventureWorks.Repository.Tests.Integration/DatabaseFixture.cs
@@ -1,6 +1,72 @@
-﻿namespace Platy.AdventureWorks.Repository.Tests.Integration;
+﻿using AutoMapper;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Images;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Platy.AdventureWorks.Repository.Domain.Mapping;
+using Testcontainers.MsSql;
 
-public class DatabaseFixture
+namespace Platy.AdventureWorks.Repository.Tests.Integration;
+
+public class DatabaseFixture : IDisposable
 {
-  
+  private IFutureDockerImage? _msSqlImage;
+  private MsSqlContainer? _msSqlContainer;
+
+  public DatabaseFixture()
+  {
+    
+    // 1. Start the container in docker using the image chriseaton/adventureworks:lates.
+    // var msSqlContainer = new MsSqlBuilder()
+    //   .WithImage("chriseaton/adventureworks:latest")
+    //   .WithEnvironment("MSSQL_SA_PASSWORD", "Test123Test*")
+    //   .WithEnvironment("ACCEPT_EULA","Y")
+    //   .WithPortBinding(1433,1433)
+    //   .WithName("trusting_ritchie")
+    //   .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Server is ready."))
+    //   .Build();
+    // msSqlContainer.StartAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+    
+    // 1. Create the image and then Start the container in docker using the Dockerfile.
+    var get = CommonDirectoryPath.GetSolutionDirectory();
+    _msSqlImage = new ImageFromDockerfileBuilder()
+      .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), "docker")
+      .WithDockerfile("Dockerfile")
+      .WithName("platy.repository.tests/adventure-works-db:0.0.1")
+      .WithCleanUp(true)
+      .WithDeleteIfExists(true)
+      .WithBuildArgument("BAK_FILE", "AdventureWorks2022.bak")
+      .Build();
+    _msSqlImage.CreateAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+    _msSqlContainer = new MsSqlBuilder()
+      .WithImage("platy.repository.tests/adventure-works-db:0.0.1")
+      .WithEnvironment("MSSQL_SA_PASSWORD", "Test123Test*")
+      .WithEnvironment("ACCEPT_EULA","Y")
+      .WithCleanUp(true)
+      .WithPortBinding(1433,1433)
+      .WithName("adventure-works-db")
+      .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Server is ready."))
+      .Build();
+    _msSqlContainer.StartAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+    
+    // 2. Get the connection string from user secrets
+    var config = new ConfigurationBuilder()
+      .AddUserSecrets<AddressRepositoryTest>()
+      .Build();
+
+    // 3. Add the adventureworks database to the service collection
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddAdventureWorkDatabase($"{config["AdventureWorksDb"]}");
+
+    // 4. Create the mapper configuration for the assembly
+    serviceCollection.AddAutoMapper(cfg => { }, typeof(AddressProfile));
+
+    ServiceProvider = serviceCollection.BuildServiceProvider();
+  }
+
+  public ServiceProvider ServiceProvider { get; set; }
+
+  public void Dispose()
+  {
+  }
 }
